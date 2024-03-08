c938bd0055613234108605c6eddcc79c99efac4a
Victor <victormojicag@correo.ugr.es>
2024-03-08 16:03:44 +0100

    conflicto

--- Ej2/src/carrera.py
+++ Ej2/src/carrera.py
@@ -6,3 +6,4 @@ class Carrera(ABC):
     def __init__(self, tipo, bicicletas=[]):
         self.tipo = tipo
         self.bicicletas = bicicletas
+        print("Se ha creado una carrera de tipo {self.tipo}")
