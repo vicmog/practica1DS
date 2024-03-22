81fc12df08ffe6011f6984f083d59dbfeddc9458
AlexRP <aleeexrp02@gmail.com>
2024-03-22 09:58:39 +0100

    Añadidos selectores como constantes en beautifulsoup

--- Ej5/main.py
+++ Ej5/main.py
@@ -1,21 +1,17 @@
-
-
 from src.selenium_strategy import SeleniumStrategy
 from src.beautiful_strategy import BeautifulSoupStrategy
-from src.context import Context
+from src.Context import Context
 
 
 def main():
-  url = 'https://finance.yahoo.com/quote/TSLA'
-  #context = Context(BeautifulSoupStrategy())
-  #result = context.scrape(url)
-  #print(result)
+    url = "https://finance.yahoo.com/quote/TSLA"
+    context = Context(BeautifulSoupStrategy())
+    result = context.scrape(url)
+    print(result)
 
-  context = Context(SeleniumStrategy())
-  result = context.scrape(url)
-  print(result)
-  
-
+    # context = Context(SeleniumStrategy())
+    # result = context.scrape(url)
+    # print(result)
 
 
 if __name__ == "__main__":
--- Ej5/src/beautiful_strategy.py
+++ Ej5/src/beautiful_strategy.py
@@ -1,4 +1,3 @@
-
 from src.scrape_strategy import ScrapeStrategy
 import requests
 
@@ -7,38 +6,51 @@ from bs4 import BeautifulSoup
 import json
 import os
 
+OPEN_VALUE_SELECTOR = {"data-test": "OPEN-value"}
+PREV_CLOSE_SELECTOR = {"data-test": "PREV_CLOSE-value"}
+VOLUME_SELECTOR = {"data-test": "TD_VOLUME-value"}
+MARKET_CAP_SELECTOR = {"data-test": "MARKET_CAP-value"}
+
+
 class BeautifulSoupStrategy(ScrapeStrategy):
     def scrape(self, url):
         data = {}
-        response = requests.get(url)
+        response = requests.get(url, headers={"Cache-Control": "no-cache"})
         if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            open_value_td = soup.find('td', {'data-test': 'OPEN-value'})
+            soup = BeautifulSoup(response.content, "html.parser")
+            open_value_td = soup.find("td", OPEN_VALUE_SELECTOR)
             if open_value_td:
                 data["open-value"] = open_value_td.text.strip()
+                print(data["open-value"])
             else:
-                return 'Open Value not found'
-            
-            prev_clouse = soup.find('td',{'data-test':'PREV_CLOSE-value'})
+                return "Open Value not found"
+
+            prev_clouse = soup.find("td", PREV_CLOSE_SELECTOR)
             if prev_clouse:
                 data["prev-clouse"] = prev_clouse.text.strip()
+                print(data["prev-clouse"])
             else:
-                return 'Prev Clouse not found'
-            
-            volume = soup.find('td',{'data-test':'TD_VOLUME-value'})
+                return "Prev Clouse not found"
+
+            volume = soup.find("td", VOLUME_SELECTOR)
             if volume:
                 data["volume"] = volume.text.strip()
+                print(data["volume"])
             else:
-                return 'Volume not found'
-            
-            market_cap = soup.find('td',{'data-test':'MARKET_CAP-value'})
+                return "Volume not found"
+
+            market_cap = soup.find("td", MARKET_CAP_SELECTOR)
             if market_cap:
-                data["market-cap"]=market_cap.text.strip()
+                data["market-cap"] = market_cap.text.strip()
+                print(data["market-cap"])
             else:
-                return 'Market Cap not found'
-            
-            with open('data-beautiful.json', 'w') as file:
-                json.dump(data,file)
+                return "Market Cap not found"
+
+            with open("data-beautiful.json", "w") as file:
+                json.dump(data, file)
                 return "Json Creado"
         else:
-            return f'Failed to retrieve the webpage, status code: {response.status_code}'
+            return (
+                f"Failed to retrieve the webpage, status code: {response.status_code}"
+            )
+
